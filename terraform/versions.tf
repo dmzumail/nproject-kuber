58699fb04f8162b0b0ea61c286e89e3bf3b89400
dmzumail <dmzumail@gmail.com>
2025-11-26 18:59:47 +0300

    fix: Откорректирован файл провайдера

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,10 @@
-# Настройка провайдера Yandex Cloud
+# versions.tf
 terraform {
   required_version = ">= 1.5"
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "~> 0.169"
+      version = ">= 0.170"
     }
   }
 }
