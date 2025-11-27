060b2fc692c72c1fc2540d937be0e5409100ebf3
dmzumail <dmzumail@gmail.com>
2025-11-27 14:03:16 +0300

    fix: Добавление роли containerregistry.images.puller в main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,10 +36,10 @@ resource "yandex_container_registry" "default" {
   name = "cr-nproject-site"
 }
 
-# 👇 РАБОЧЕЕ РЕШЕНИЕ: даём доступ к реестру через папковую IAM-роль
+# РАБОЧЕЕ РЕШЕНИЕ: даём доступ к реестру через папковую IAM-роль
 resource "yandex_resourcemanager_folder_iam_member" "k8s_sa_registry_reader" {
   folder_id = var.yc_folder_id
-  role      = "container-registry.reader"
+  role      = "containerregistry.images.puller"
   member    = "serviceAccount:${yandex_iam_service_account.k8s_sa.id}"
 }
 
